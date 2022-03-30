acd32f091ee09658344496af8be541ea85490854
Mihail Korotaev <misha@DESKTOP-C4VN1I3.localdomain>
2022-03-30 20:49:29 +0300

    add banners entite

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\BannerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    private BannerRepository $bannerRepository;
+
+    public function __construct(
+        BannerRepository $bannerRepository,
+    )
+    {
+        $this->bannerRepository = $bannerRepository;
+    }
+
     #[Route("/")]
     public function index(): Response
     {
         return $this->render('index.html.twig', [
-            'banners' => [],
+            'banners' => $this->bannerRepository->findAll(),
             'categories' => [],
             'brands' => [],
             'products' => [],
---
+++ src/Entity/Banner.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Banner
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $title;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $subtitle = null;
+
+    #[ORM\Column(type: 'string')]
+    private string $description;
+
+    #[ORM\Column(type: 'string')]
+    private string $image;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getSubtitle(): ?string
+    {
+        return $this->subtitle;
+    }
+
+    public function setSubtitle(?string $subtitle): void
+    {
+        $this->subtitle = $subtitle;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
+}
