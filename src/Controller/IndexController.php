0c8dd8e82e27945ccc394c41694505d845c4ce2e
Mihail Korotaev <misha@DESKTOP-C4VN1I3.localdomain>
2022-04-10 19:45:31 +0300

    brand and category index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\BannerRepository;
+use App\Repository\BrandRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     private BannerRepository $bannerRepository;
+    private BrandRepository $brandRepository;
+    private CategoryRepository $categoryRepository;
 
     public function __construct(
         BannerRepository $bannerRepository,
+        BrandRepository $brandRepository,
+        CategoryRepository $categoryRepository,
     )
     {
         $this->bannerRepository = $bannerRepository;
+        $this->brandRepository = $brandRepository;
+        $this->categoryRepository = $categoryRepository;
     }
 
     #[Route("/")]
@@ -23,8 +31,8 @@ class IndexController extends AbstractController
     {
         return $this->render('index.html.twig', [
             'banners' => $this->bannerRepository->findAll(),
-            'categories' => [],
-            'brands' => [],
+            'categories' => $this->categoryRepository->findAll(),
+            'brands' => $this->brandRepository->findAll(),
             'products' => [],
         ]);
     }
---
+++ src/Entity/Brand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Brand
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $title;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+}
